494eb64862eac79b1d446c01cee645dbadb48f3d
nari.ku <nari.ku@linecorp.com>
2022-06-15 21:46:12 +0900

    test

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  useTabs: false,
+  semi: true,
+  arrowParens: 'always',
+  requirePragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'ignore',
+  parser: 'typescript',
+  endOfLine: 'auto',
+  trailingComma: 'es5',
+};
